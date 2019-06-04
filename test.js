0cc3f73a0b397597f786c223d2cb0e80eb21cff5
xiong_x <xiong_x@Ctrip.com>
2019-06-04 11:47:14 +0800

    update

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
-b = ["f", "O", "o", "!"]
-console.log(b.reverse()); // ["!","o","O","f"]
+function test(list) {
+    var arr = [];
+    console.log(list)
+    list.map(item => {
+        //获取当前文件的绝对路径
+        if (item instanceof Array) {
+            arr = arr.concat(test(item));
+        } else {
+            arr.push(item);
+        }
+    })
+    return arr;
+}
+
+console.log(test([1, [2 ,[3, 4]], 5]))
