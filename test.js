6399680f59d36511cacfd97d40d090b9f01eb630
bearnew <651831513@qq.com>
2019-11-11 23:33:20 +0800

    update

--- test.js
+++ test.js
@@ -1,159 +1,75 @@
 new Promise((resolve, reject) => {
     setTimeout(() => {
-        console.log('1111', resolve);
         resolve({ test: 1 })
-    }, 1000)
+        resolve({ test: 2 })
+        // reject({ test: 2 })
+    }, 100)
 }).then((data) => {
     console.log('result1', data)
-}).catch((ex) => {
-    console.log('error', ex)
+}, (data1) => {
+    console.log('result2', data1)
 })
+// }).then((data) => {
+//     console.log('result3', data)
+// })
 
 function Promise(fn) {
-    let state = 'pending'
-    let value = null
-    const callbacks = []
+    let state = 'pending';
+    let value = null;
+    const callbacks = [];
 
-    this.then = function (onFulfilled, onRejected) {
+    this.then = function (onFulfilled) {
+        console.log('3333')
         return new Promise((resolve, reject) => {
-            handle({
+            console.log('4444')
+            handle({ //桥梁，将新 Promise 的 resolve 方法，放到前一个 promise 的回调对象中
                 onFulfilled,
-                onRejected,
-                resolve,
-                reject,
+                resolve
             })
         })
     }
 
-    this.catch = function (onError) {
-        this.then(null, onError)
-    }
-
-    this.finally = function (onDone) {
-        this.then(onDone, onError)
-    }
-
-    this.resolve = function (value) {
-        if (value && value instanceof Promise) {
-            return value
-        } if (value && typeof value === 'object' && typeof value.then === 'function') {
-            const { then } = value
-            return new Promise((resolve) => {
-                then(resolve)
-            })
-        } if (value) {
-            return new Promise(resolve => resolve(value))
-        }
-        return new Promise(resolve => resolve())
-    }
-
-    this.reject = function (value) {
-        return new Promise(((resolve, reject) => {
-            reject(value)
-        }))
-    }
-
-    this.all = function (arr) {
-        const args = Array.prototype.slice.call(arr)
-        return new Promise(((resolve, reject) => {
-            if (args.length === 0) return resolve([])
-            let remaining = args.length
-
-            function res(i, val) {
-                try {
-                    if (val && (typeof val === 'object' || typeof val === 'function')) {
-                        const { then } = val
-                        if (typeof then === 'function') {
-                            then.call(val, (val) => {
-                                res(i, val)
-                            }, reject)
-                            return
-                        }
-                    }
-                    args[i] = val
-                    if (--remaining === 0) {
-                        resolve(args)
-                    }
-                } catch (ex) {
-                    reject(ex)
-                }
-            }
-            for (let i = 0; i < args.length; i++) {
-                res(i, args[i])
-            }
-        }))
-    }
-
-    this.race = function (values) {
-        return new Promise(((resolve, reject) => {
-            for (let i = 0, len = values.length; i < len; i++) {
-                values[i].then(resolve, reject)
-            }
-        }))
-    }
-
     function handle(callback) {
         if (state === 'pending') {
+            console.log('6666')
             callbacks.push(callback)
-            return
+            return;
         }
 
-        const cb = state === 'fulfilled' ? callback.onFulfilled : callback.onRejected
-        const next = state === 'fulfilled' ? callback.resolve : callback.reject
-
-        if (!cb) {
-            next(value)
-            return
-        }
-        try {
-            const ret = cb(value)
-            next(ret)
-        } catch (e) {
-            callback.reject(e)
+        if (state === 'fulfilled') {
+            console.log('7777', callback)
+            if (!callback.onFulfilled) {
+                callback.resolve(value)
+                return;
+            }
+            const ret = callback.onFulfilled(value) //处理回调
+            console.log('555555', ret)
+            callback.resolve(ret) //处理下一个 promise 的resolve
         }
     }
     function resolve(newValue) {
+        console.log('ssss')
         const fn = () => {
+            console.log('999', state)
             if (state !== 'pending') return
 
-            if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
-                const { then } = newValue
-                if (typeof then === 'function') {
-                    // newValue 为新产生的 Promise,此时resolve为上个 promise 的resolve
-                    // 相当于调用了新产生 Promise 的then方法，注入了上个 promise 的resolve 为其回调
-                    then.call(newValue, resolve, reject)
-                    return
-                }
-            }
-            state = 'fulfilled'
+            state = 'fulfilled';
             value = newValue
+            console.log('111111')
             handelCb()
         }
 
-        setTimeout(fn, 0)
+        setTimeout(fn, 0) //基于 PromiseA+ 规范
     }
-    function reject(error) {
-        const fn = () => {
-            if (state !== 'pending') return
 
-            if (error && (typeof error === 'object' || typeof error === 'function')) {
-                const { then } = error
-                if (typeof then === 'function') {
-                    then.call(error, resolve, reject)
-                    return
-                }
-            }
-            state = 'rejected'
-            value = error
-            handelCb()
-        }
-        setTimeout(fn, 0)
-    }
     function handelCb() {
+        console.log('22222', callbacks.length, callbacks)
         while (callbacks.length) {
-            const fn = callbacks.shift()
-            handle(fn)
-        }
+            const fulfiledFn = callbacks.shift();
+            handle(fulfiledFn);
+        };
     }
-    fn(resolve, reject)
+
+    console.log('uuuuu')
+    fn(resolve)
 }
