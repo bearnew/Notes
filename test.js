dc09562ef99b75d2027c66b363d7f8f4d3363ea0
bearnew <651831513@qq.com>
2020-01-04 14:24:48 +0800

    update

--- test.js
+++ test.js
@@ -1,11 +1,40 @@
 const list = [4, 2, 6, 5, 7, 3, 1];
-console.log(quickSort(list)); // [1, 2, 3, 4, 5, 6, 7]
+console.log(mergeSort(list)); // [1, 2, 3, 4, 5, 6, 7]
 
-function quickSort(arr) {
-    if (arr.length === 0) return arr;
+function mergeSort(arr) {
+    var length = arr.length;
+    if (length === 1) {
+        return arr;
+    }
+    var mid = Math.floor(length / 2),
+        left = arr.slice(0, mid),
+        right = arr.slice(mid, length);
 
-    let pivot = arr[arr.length - 1]
-    let left = arr.filter((v, i) => (v <= pivot) && (i !== arr.length - 1))
-    let right = arr.filter(v => v > pivot)
-    return [...quickSort(left), pivot, ...quickSort(right)]
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+    var result = [];
+    var leftIndex = 0;
+    var rightIndex = 0;
+
+    console.log(left, right)
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex++]);
+        } else {
+            result.push(right[rightIndex++]);
+        }
+    }
+
+    while (leftIndex < left.length) {
+        result.push(left[leftIndex++]);
+    }
+
+    while (rightIndex < right.length) {
+        result.push(right[rightIndex++]);
+    }
+
+    console.log(result)
+    return result;
 }
