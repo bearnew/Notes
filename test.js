9b3b6040b86f3d681ad787b2c47f304c959ad442
bearnew <651831513@qq.com>
2020-02-09 22:09:48 +0800

    update

--- test.js
+++ test.js
@@ -1,37 +1,21 @@
-const list = [5, 4, 3, 2, 1];
-console.log(binarySearch(list, 3)); // 2
+console.log(knapSack(6, [3, 4, 5], [2, 3, 4]));
 
-function binarySearch(list, item) {
-    const arr = quickSort(list);
+// 背包价值
+function knapSack(capacity, values, weights) {
+    var n = values.length;
+    var load = 0;
+    var i = 0;
+    var val = 0;
 
-    let low = 0;
-    let high = arr.length - 1;
-    let mid;
-    let element;
-
-    while (low <= high) {
-        mid = Math.floor((low + high) / 2);
-        element = arr[mid];
-
-        if (element < item) {
-            low = mid + 1;
-        } else if (element > item) {
-            high = mid - 1;
+    for (i = 0; i < n && load < capacity; i++) {
+        if (weights[i] <= (capacity - load)) {
+            val += values[i];
+            load += weights[i];
         } else {
-            return mid;
+            var r = (capacity - load) / weights[i];
+            val += r * values[i];
+            load += r * weights[i];
         }
     }
-
-    return -1;
+    return val;
 }
-
-function quickSort(arr) {
-    if (arr.length === 0) return arr;
-
-    let pivot = arr[arr.length - 1]
-    // 通过filter筛选，这个过程不仅在分区 也在进行排序
-    let left = arr.filter((v, i) => (v <= pivot) && (i !== arr.length - 1))
-    let right = arr.filter(v => v > pivot)
-    return [...quickSort(left), pivot, ...quickSort(right)]
-}
-
