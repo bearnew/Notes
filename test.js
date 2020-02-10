503b6267c6ebe4458203348c64f55ba94b122af9
bearnew <651831513@qq.com>
2020-02-10 12:10:48 +0800

    update

--- test.js
+++ test.js
@@ -1,21 +1,31 @@
-console.log(knapSack(6, [3, 4, 5], [2, 3, 4]));
+var values = [3, 4, 5],
+    weights = [2, 3, 4],
+    capacity = 5,
+    n = values.length;
+
+console.log(knapSack(capacity, weights, values, n));
 
 // 背包价值
-function knapSack(capacity, values, weights) {
-    var n = values.length;
-    var load = 0;
-    var i = 0;
-    var val = 0;
+function knapSack(capacity, weights, values, n) {
+    var i, w, a, b, ks = [];
 
-    for (i = 0; i < n && load < capacity; i++) {
-        if (weights[i] <= (capacity - load)) {
-            val += values[i];
-            load += weights[i];
-        } else {
-            var r = (capacity - load) / weights[i];
-            val += r * values[i];
-            load += r * weights[i];
+    for (i = 0; i <= n; i++) {
+        ks[i] = [];
+    }
+
+    for (i = 0; i <= n; i++) {
+        for (w = 0; w <= capacity; w++) {
+            if (i === 0 || w === 0) {
+                ks[i][w] = 0;
+            } else if (weights[i - 1] <= w) {
+                a = values[i - 1] + ks[i - 1][w - weights[i - 1]]
+                b = ks[i - 1][w];
+                ks[i][w] = (a > b) ? a : b;
+            } else {
+                ks[i][w] = ks[i - 1][w];
+            }
         }
     }
-    return val;
+
+    return ks[n][capacity];
 }
