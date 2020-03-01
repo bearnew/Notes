bcdfef84d4aacda750078aeb0d26b87fc2a720a6
bearnew <651831513@qq.com>
2020-03-01 20:46:58 +0800

    update

--- test.js
+++ test.js
@@ -1,13 +1,25 @@
-$.fn.extend({
-    check: function() {
-        console.log('1111')
-        return this.each(function() {
-            this.checked = true;
-        });
-    },
-    uncheck: function() {
-        return this.each(function() {
-            this.checked = false;
-        });
+function getClimbingWays(n) {
+    if (n < 1) {
+        return 0;
     }
-});
+    if (n === 1) {
+        return 1;
+    }
+    if (n === 2) {
+        return 2;
+    }
+
+    var a = 1;
+    var b = 2;
+    var temp = 0;
+
+    for (var i = 3; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return temp;
+}
+
+console.log(getClimbingWays(10))
