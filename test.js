087291e808439b1c1e66c44543e81b7f98cdc547
xx熊新 <xiong_x@Ctrip.com>
2020-03-03 10:20:50 +0800

    update

--- test.js
+++ test.js
@@ -1,25 +1,28 @@
-function getClimbingWays(n) {
-    if (n < 1) {
-        return 0;
-    }
-    if (n === 1) {
-        return 1;
-    }
-    if (n === 2) {
-        return 2;
-    }
-
-    var a = 1;
-    var b = 2;
-    var temp = 0;
-
-    for (var i = 3; i <= n; i++) {
-        temp = a + b;
-        a = b;
-        b = temp;
-    }
-
-    return temp;
+var Parent = {
+	name: 'james',
+	sayHi: function() {
+		console.log(`Hi ${this.name}.`)
+	}
 }
 
-console.log(getClimbingWays(10))
+var Child = function() {
+	this.getName = function() {
+		console.log(this.name)
+	}
+}
+
+// 相当于把Child.prototype.__proto__指向Parent
+Child.prototype = Object.create(Parent, {
+	name: {
+		value: 'wade'
+	}
+});
+var c = new Child();
+
+console.log(Child.prototype)
+console.log(c.constructor) // ƒ Object() { [native code] }
+Child.prototype.constructor = Child;
+console.log(c.constructor) // f () { this.getName = function() { console.log(this.name) } }
+
+c.sayHi(); // Hi wade.
+c.getName(); // wade
