8d3e1a61a96abc572e956a2af96f5544a7f33f09
bearnew <651831513@qq.com>
2020-03-08 19:19:15 +0800

    update

--- test.js
+++ test.js
@@ -1,31 +1,7 @@
-console.log(coinChange([5, 1, 10, 25], 6)); // [1, 10, 25]
+var x = '123';
 
-function coinChange(coins, amount) {
-    var memo = [];
-    return dp(amount);
-
-    function dp(n) {
-        if (memo[n]) return memo[n];
-        if (n === 0) return [];
-        if (n < 0) return null;
-
-        var min = Number.MAX_SAFE_INTEGER;
-        var minCoins;
-        var subCoins;
-
-        for (let coin of coins) {
-            if (n - coin < 0) continue;
-            var subCoins = dp(n - coin);
-
-            if (!subCoins) continue;
-
-            if (subCoins.length + 1 < min) {
-                minCoins = [coins].concat(subCoins);
-                min = subCoins.length + 1;
-            }
-        }
-
-        return (memo[n] = minCoins)
-    }
-
-}
+console.log(~~x); // 123
+console.log(+x); // 123
+console.log(Number(x)); // 123
+console.log(parseInt(x)); // 123
+console.log(parseFloat(x)); // 123
