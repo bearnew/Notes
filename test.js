a3aa75fb734c145f02a157a1d314f1cfb45d4871
bearnew <651831513@qq.com>
2020-03-14 11:08:32 +0800

    update

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-function Test() {
-    this.a = 1;
-    return {
-        x: 1
-    }
+function setName(obj) {
+    obj.name = 'bear_new';
+    obj = new Object();   // 此时给obj重新赋值新变量，将obj变成了局部对象，即使修改，原始的引用仍然保持未变，并且局部对象会在函数执行完后立即被销毁
+    obj.name = 'Greg';
 }
 
-var a = new Test();
-console.log(a)
+var person = new Object();
+setName(person);    // 函数内的参数obj变化，函数外部的person也会发生改变，obj和person引用同一对象，指向的对象在堆内存中只有一个，而且是全局对象
+alert(person.name);    // 'bear_new'
