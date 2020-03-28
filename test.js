2148dbd69ae416a11e285cf74f3ea983d9045677
bearnew <651831513@qq.com>
2020-03-28 14:21:53 +0800

    update

--- test.js
+++ test.js
@@ -1,18 +1,5 @@
-function unique1(arr) {
-    return Array.from(new Set(arr));
-}
-
-function unique2(arr) {
-    var map = {};
-    arr.map((i, index) => {
-        if (map[i]) {
-            arr.splice(index, 1);
-        } else {
-            map[i] = true;
-        }
-    })
-
-    return arr;
-}
-
-console.log(unique2([1, 2, 2, 3, 4]))
+var a = { x: 1 }
+var b = a;
+console.log(b)
+a = null;
+console.log(a, b)
