5c8b7893dd736a1c3a00fd8cfa0c9f7dd8db39e0
bearnew <651831513@qq.com>
2020-09-08 20:53:13 +0800

    update

--- test.js
+++ test.js
@@ -1,29 +1,10 @@
-let timer;
-
-function testApi(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(3000)
-        }, 3000);
-        // resolve(3000)
-    })
+var a = 1;
+switch(true) {
+    case a === 2:
+        default:
+            console.log('1111');
+            break;
+    case a === 1:
+        console.log('2222');
+        break;
 }
-
-function polling(pollingTime) {
-    timer = setTimeout(() => {
-        console.log('start', timer)
-        testApi().then(res => {
-            console.log('end')
-            polling(res);
-        })
-    }, pollingTime);
-}
-
-polling(0);
-
-setTimeout(() => {
-    // 此时清空伦序，timer清除掉了，但是testApi已经加入到了事件队列
-    // testApi返回后，轮询继续执行
-    clearTimeout(timer);
-    console.log('clear', timer)
-}, 7000)
