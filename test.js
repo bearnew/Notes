d1c4bd9b134c65d7c9569a9f5b56f5bcb4c5c751
bearnew <651831513@qq.com>
2020-12-23 11:41:04 +0800

    update

--- test.js
+++ test.js
@@ -1,38 +1,11 @@
-"use strict";
-exports.__esModule = true;
-exports.TypedEvent = void 0;
-var TypedEvent = /** @class */ (function () {
-    function TypedEvent() {
-        var _this = this;
-        this.listeners = [];
-        this.listenersOncer = [];
-        this.on = function (listener) {
-            _this.listeners.push(listener);
-            return {
-                dispose: function () { return _this.off(listener); }
-            };
-        };
-        this.once = function (listener) {
-            _this.listenersOncer.push(listener);
-        };
-        this.off = function (listener) {
-            var callbackIndex = _this.listeners.indexOf(listener);
-            if (callbackIndex > -1)
-                _this.listeners.splice(callbackIndex, 1);
-        };
-        this.emit = function (event) {
-            _this.listeners.forEach(function (listener) { return listener(event); });
-            _this.listenersOncer.forEach(function (listener) { return listener(event); });
-            _this.listenersOncer = [];
-        };
-        this.pipe = function (te) {
-            console.log('11111');
-            return _this.on(function (e) { return te.emit(e); });
-        };
+var arr = [1,2,3]
+
+function test() {
+    for (const num of arr) {
+        if (num > 2) {
+            return true;
+        }
     }
-    return TypedEvent;
-}());
-exports.TypedEvent = TypedEvent;
-var te = new TypedEvent();
-var te1 = new TypedEvent();
-te.pipe(te1);
+}
+
+console.log(test())
