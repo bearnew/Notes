d6d2d4d2d8b395fcabc9b4053d46c731573994b8
bearnew <651831513@qq.com>
2021-01-25 11:32:07 +0800

    update

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
-var arr = [1,2,3]
-
-function test() {
-    for (const num of arr) {
-        if (num > 2) {
-            return true;
-        }
-    }
-}
-
-console.log(test())
+function test(a, b = 1) {
+    arguments[0] = 20;
+    console.log(a);            // 123
+  }
+  console.log(test.length);    // 1
+  
+  test(123); // 123
