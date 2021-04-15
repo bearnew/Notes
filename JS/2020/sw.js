e69353723300855101ae14b836b5d1a7054e377f
bearnew <651831513@qq.com>
2021-04-15 23:45:57 +0800

    update

--- JS/2020/sw.js
+++ JS/2020/sw.js
@@ -1,6 +1,8 @@
 if (navigator && ('serviceWorker' in navigator)) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/poker_sw.js').then(registration => {
+        navigator.serviceWorker.register('/poker_sw.js', {
+            scope: '/poker/'
+        }).then(registration => {
             console.log('SW registered: ', registration);
         }).catch(registrationError => {
             console.log('SW registration failed: ', registrationError);
