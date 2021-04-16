b2162ac0f553a0a44cd0e053f88c65ada345946a
bearnew <651831513@qq.com>
2021-04-16 14:08:25 +0800

    update

--- JS/2020/sw.js
+++ JS/2020/sw.js
@@ -1,3 +1,4 @@
+// service worker参考文章: https://lavas-project.github.io/pwa-book/chapter04/2-service-worker-register.html
 if (navigator && ('serviceWorker' in navigator)) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/poker_sw.js', {
