62af5f72a40b776b8883afd5498f253721cd3c31
bearnew <651831513@qq.com>
2021-05-02 02:30:24 +0800

    update

--- test.js
+++ test.js
@@ -1,96 +1,53 @@
-let isMount = true;
-let workInProgressHook = null;
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+ var solveNQueens = function(n) {
+  var board = new Array(n);
+  for (var i = 0; i < n; i++) {
+      board[i] = new Array(n).fill('.');
+  }
 
-const fiber = {
-  stateNode: App,
-  memorizeState: null,
+  var cols = new Set(); // 列
+  var diag1 = new Set(); // 正对角线
+  var diag2 = new Set(); // 反对角线
+  var res = [];
+
+  var helper = function(row) {
+      if (row === n) {
+          var stringsBoard = board.slice();
+          for (let i = 0; i < n; i++) {
+              stringsBoard[i] = stringsBoard[i].join('');
+          }
+          res.push(stringsBoard);
+          return;
+      }
+
+      for (let col = 0; col < n; col++) {
+          if (!cols.has(col) && !diag1.has(row + col) && !diag2.has(row - col)) {
+
+              board[row][col] = 'Q';
+              cols.add(col);
+              diag1.add(row + col);
+              diag2.add(row - col);
+              
+              // 执行2，行3，行4
+              helper(row + 1);
+
+              // 行4执行完会
+              board[row][col] = '.';  // 撤销该点的皇后
+              cols.delete(col);       // 对应的记录也删一下
+              diag1.delete(row + col);
+              diag2.delete(row - col);
+
+          }
+      }
+  }
+
+  helper(0);
+
+  console.log('fffffff', res)
+  return res;
 };
 
-function useState(initialState) {
-  let hook;
-
-  if (isMount) {
-    hook = {
-      memorizeState: initialState,
-      next: null,
-      queue: {
-        pending: null,
-      },
-    };
-
-    if (!fiber.memorizeState) {
-      fiber.memorizeState = hook;
-      workInProgressHook = hook;
-    } else {
-      workInProgressHook.next = hook;
-    }
-    workInProgressHook = hook;
-  } else {
-    hook = workInProgressHook;
-    workInProgressHook = workInProgressHook.next;
-  }
-
-  let baseState = hook.memorizeState;
-  if (hook.queue.pending) {
-    let firstUpdate = hook.queue.pending.next;
-
-    do {
-      const action = firstUpdate.action;
-      baseState = action(baseState);
-      firstUpdate = firstUpdate.next;
-    } while (firstUpdate !== hook.queue.pending.next);
-
-    hook.queue.pending = null;
-  }
-
-  hook.memorizeState = baseState;
-  return [baseState, dispatchAction.bind(null, hook.queue)];
-}
-
-function dispatchAction(queue, action) {
-  const update = {
-    action,
-    next: null,
-  };
-
-  if (queue.pending === null) {
-    update.next = update;
-  } else {
-    // 环状链表
-    update.next = queue.pending.next;
-    queue.pending.next = update;
-  }
-
-  queue.pending = update;
-
-  schedule();
-}
-
-// 调度
-function schedule() {
-  workInProgressHook = fiber.memorizeState;
-  const app = fiber.stateNode();
-  isMount = false;
-
-  return app;
-}
-
-function App() {
-  const [num, updateNum] = useState(0);
-  const [num1, updateNum1] = useState(10);
-
-  console.log("isMount", isMount);
-  console.log("num", num);
-  console.log("num1", num1);
-
-  return {
-    onClick() {
-      updateNum((num) => num + 1);
-    },
-    onFocus() {
-      updateNum1((num) => num + 10);
-    },
-  };
-}
-
-window.app = schedule();
+solveNQueens(4)
