1b269c07722e086de3f9ca552cd14076e4ea4042
bearnew <651831513@qq.com>
2022-06-11 12:24:05 +0800

    feat: 错误处理

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
-try {
-    console.log("step1");
-    throw EvalError("2222");
-} catch (error) {
-    console.log("step2");
-} finally {
-    console.log("step3");
-}
+const net = require("net");
+const server1 = net.createServer().listen(80, "127.0.0.1");
+server1.on("listening", function () {
+    console.log("Server listening!");
+});
+
+const server2 = net.createServer().listen(80, "127.0.0.1");
+server2.on("listening", function () {
+    console.log("Server listening!");
+});
+
+server2.on("error", function (error) {
+    console.error("1111", error.message); // 1111 listen EADDRINUSE: address already in use 127.0.0.1:80
+});
