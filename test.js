cc139f31965cfa9bdca5ee10ec63cc6be88a5b1e
bearnew <651831513@qq.com>
2023-05-12 01:06:37 +0800

    update

--- test.js
+++ test.js
@@ -1,11 +1,58 @@
-function test() {
-  var a = 0;
-  while (1) {
-      switch (a) {
-          case 0:
-              console.log(this);
-              break;
-      }
-  }
+var s = "ADOBECODEBANC";
+var t = "ABC";
+
+console.log(minWindow(s, t));
+
+function minWindow(s, t) {
+    var winFreq = {};
+    var tFreq = {};
+    var left = 0;
+    var right = 0;
+    var begin = 0;
+    var minLen = s.length + 1;
+    var distance = 0;
+
+    for (var str of t) {
+        if (!tFreq[str]) {
+            tFreq[str] = 1;
+        } else {
+            tFreq[str] += 1;
+        }
+        winFreq[str] = 0;
+    }
+
+    while (right < s.length) {
+        if (tFreq[s[right]] === 0) {
+            right++;
+        } else {
+            if (winFreq[s[right]] < tFreq[s[right]]) {
+                distance++;
+            }
+            winFreq[s[right]]++;
+            right++;
+            while (distance === t.length) {
+                if (right === t.length) {
+                    if (right - left < minLen) {
+                        minLen = right - left;
+                        begin = left;
+                    }
+                    if (winFreq[s[left]] === 0) {
+                        left++;
+                    } else {
+                        if (winFreq[s[left]] === tFreq[s[left]]) {
+                            distance--;
+                        }
+                        winFreq[s[left]]--;
+                        left++;
+                    }
+                }
+            }
+        }
+    }
+
+    if (minLen > s.length) {
+        return "";
+    } else {
+        return s.slice(begin, begin + minLen);
+    }
 }
-test();
