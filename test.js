565a37f1bf529bafa5be8b71ba81e245affb831d
bearnew <651831513@qq.com>
2023-05-14 00:55:27 +0800

    update

--- test.js
+++ test.js
@@ -1,58 +1,56 @@
-var s = "ADOBECODEBANC";
-var t = "ABC";
+var exsit = function (board, word) {
+    const h = board.length; // 有几行
+    const w = board[0].length; // 有几列
+    const directions = [
+        [0, 1], // 右
+        [0, -1], // 左
+        [1, 0], // 下
+        [-1, 0], // 上
+    ];
 
-console.log(minWindow(s, t));
-
-function minWindow(s, t) {
-    var winFreq = {};
-    var tFreq = {};
-    var left = 0;
-    var right = 0;
-    var begin = 0;
-    var minLen = s.length + 1;
-    var distance = 0;
-
-    for (var str of t) {
-        if (!tFreq[str]) {
-            tFreq[str] = 1;
-        } else {
-            tFreq[str] += 1;
-        }
-        winFreq[str] = 0;
+    const visited = new Array(h); // 标记是否访问过数组
+    for (let i = 0; i < visited.length; i++) {
+        // 初始化
+        visited[i] = new Array(w).fill(false);
     }
 
-    while (right < s.length) {
-        if (tFreq[s[right]] === 0) {
-            right++;
+    const check = (i, j, s, k) => {
+        // 从i j出发，搜索s中从k开始的字符
+        if (board[i][j] !== s[k]) {
+            return false;
+        } else if (k === s.length - 1) {
+            // 搜索到了word中的最后1位
+            return true;
         } else {
-            if (winFreq[s[right]] < tFreq[s[right]]) {
-                distance++;
-            }
-            winFreq[s[right]]++;
-            right++;
-            while (distance === t.length) {
-                if (right === t.length) {
-                    if (right - left < minLen) {
-                        minLen = right - left;
-                        begin = left;
-                    }
-                    if (winFreq[s[left]] === 0) {
-                        left++;
-                    } else {
-                        if (winFreq[s[left]] === tFreq[s[left]]) {
-                            distance--;
+            visited[i][j] = true; // 访问过
+            let result = false;
+            for (const [dx, dy] of directions) {
+                let newi = i + dx;
+                let newj = j + dy;
+                if (newi >= 0 && newi < h && newj >= 0 && newj < w) {
+                    // 新坐标位置合法
+                    if (!visited[newi][newj]) {
+                        const flag = check(newi, newj, s, k + 1);
+                        if (flag) {
+                            result = true;
+                            break;
                         }
-                        winFreq[s[left]]--;
-                        left++;
                     }
                 }
             }
+
+            visited[i][j] = false; // 回溯状态
+            return result;
+        }
+    };
+
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            const flag = check(i, j, word, 0);
+            if (flag) {
+                return true;
+            }
         }
     }
-
-    if (minLen > s.length) {
-        return "";
-    } else {
-        return s.slice(begin, begin + minLen);
-    }
-}
+    return false;
+};
